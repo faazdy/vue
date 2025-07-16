8fa7c09613d51a411be4328bf8a4dc640d59c817
faazdy <leorengifor@gmail.com>
2025-07-16 00:01:53 -0500

    Learn v-for in Vue 3 with a simple store products example.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
-<script setup> /*SETUP sirve para saltarnos la parte de 'export default...' */
-  import HelloWorld from './components/HelloWorld.vue'
-  import CardwithImage from './projects/CardwithLikes.vue'
+<script setup>/*SETUP sirve para saltarnos la parte de 'export default...' */
+import HelloWorld from './components/HelloWorld.vue'
+
+
 
 </script>
 
@@ -9,11 +10,3 @@
   <HelloWorld />
 </template>
 
-<style scoped> /*SCPED sirve para mantener los estilos nomas para el componente actual*/ 
-  *{
-    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-  body{
-    display: flex;
-  }
-</style>
---
+++ src/projects/StoreProducts/data.js
@@ -0,0 +1,23 @@
+const products = [
+  {
+    id: 1,
+    title: 'Camiseta Oversize',
+    price: 25.99,
+    description: 'Camiseta de algodón orgánico, ideal para clima cálido.',
+    image: 'https://plus.unsplash.com/premium_photo-1718913936342-eaafff98834b?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dCUyMHNoaXJ0fGVufDB8fDB8fHww'
+  },
+  {
+    id: 2,
+    title: 'Zapatos deportivos',
+    price: 79.99,
+    description: 'Perfectos para correr, caminar y vestir casual.',
+    image: 'https://cdn.pixabay.com/photo/2016/11/19/18/06/feet-1840619_640.jpg'
+  },
+  {
+    id: 3,
+    title: 'Reloj inteligente',
+    price: 199.99,
+    description: 'Monitorea tu ritmo cardíaco y pasos con estilo.',
+    image: 'https://i.blogs.es/6804b2/colores/450_1000.jpeg'
+  }
+]
